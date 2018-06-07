23ab9c9c039132710d121ec81b46074ee8318731
negtv2postv <39930370+negtv2postv@users.noreply.github.com>
2018-06-07 15:03:06 -0500

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,28 @@
+class Fruit {
+    constructor(name, color) {
+        this._name = name;
+        this._color = color;
+        this._flavor = 10;
+    }
+    get name() {
+        return this._name;
+    }
+    get color() {
+        return this._color;
+    }
+   
+    ripe() {
+        this._name = true;
+        console.log(`fruit is ripe ${this._name}`);
+    }
+    flavor(flavor) {
+        this._flavor += flavor;
+        console.log(`it taste tangy ${this._flavor}`);
+    }
+}
+var Orange = new Fruit("Florida", "Orange");
+
+console.log(Orange.name);
+console.log(Orange.color);
+Orange.flavor(10);
+Orange.ripe();
